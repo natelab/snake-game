e40a81084f16a48a7c262a50d34965213bde0a2b
Nathan <122789063+natelab@users.noreply.github.com>
2025-11-18 21:26:36 +0200

    Bonus snake food and modern design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
@@ -40,10 +29,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(23)))), ((int)(((byte)(42)))));
+            this.pictureBox1.Location = new System.Drawing.Point(20, 20);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(541, 560);
+            this.pictureBox1.Size = new System.Drawing.Size(560, 560);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.updateGraphics);
@@ -51,47 +40,54 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(665, 50);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(163)))), ((int)(((byte)(184)))));
+            this.label1.Location = new System.Drawing.Point(620, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 29);
+            this.label1.Size = new System.Drawing.Size(92, 37);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Score:";
+            this.label1.Text = "Score";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(774, 50);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(197)))), ((int)(((byte)(94)))));
+            this.label2.Location = new System.Drawing.Point(620, 80);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 29);
+            this.label2.Size = new System.Drawing.Size(100, 59);
             this.label2.TabIndex = 2;
             this.label2.Text = "000";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(215, 226);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 24);
+            this.label3.Size = new System.Drawing.Size(95, 25);
             this.label3.TabIndex = 3;
             this.label3.Text = "End Text";
+            this.label3.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(909, 631);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(800, 600);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Simple Snake Game";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Modern Snake Game";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +13,28 @@ namespace Snake_Game
 {
     public partial class Form1 : Form
     {
-        private List<Circle> Snake = new List<Circle>(); //A list for the body of the snake
-        private Circle food = new Circle(); //The food for the snake
+        private List<Circle> Snake = new List<Circle>(); //List for the body of the snake
+        private Circle food = new Circle(); //Normal food (plus 1)
+        private Circle bonusFood = new Circle(); // Plus 5 Food and not the normal one
+        private bool bonusFoodActive = false;
+        private int bonusFoodTimer = 0;
+        private Random rnd = new Random();
+
         public Form1()
         {
             InitializeComponent();
-            new Settings(); //Linking setting to this class of ours
 
-            gameTimer.Interval = 1000 / Settings.Speed; // Changing of the game timers to the settings speed
+            
+            this.BackColor = Color.FromArgb(26, 26, 46); //Dark background
+            this.DoubleBuffered = true;
+
+            new Settings();
+
+            gameTimer.Interval = 1000 / Settings.Speed;
             gameTimer.Tick += updateScreen;
             gameTimer.Start();
 
             startGame();
-
         }
 
         private void keyisdown(object sender, KeyEventArgs e)
@@ -40,48 +50,132 @@ namespace Snake_Game
         private void updateGraphics(object sender, PaintEventArgs e)
         {
             Graphics canvas = e.Graphics;
+            canvas.SmoothingMode = SmoothingMode.AntiAlias;
 
-            if(Settings.GameOver == false)
+            if (Settings.GameOver == false)
             {
-                //If the game has not ended
-
-                Brush snakeColour;
-
-                for(int i = 0; i < Snake.Count; i++)
+                // Draw snake with modern gradient
+                for (int i = 0; i < Snake.Count; i++)
                 {
+                    Rectangle rect = new Rectangle(
+                        Snake[i].X * Settings.Width,
+                        Snake[i].Y * Settings.Height,
+                        Settings.Width, Settings.Height);
+
                     if (i == 0)
                     {
-                        snakeColour = Brushes.Black; //Head of the snake is black
+                        // Head - gradient from dark to light green
+                        using (LinearGradientBrush brush = new LinearGradientBrush(
+                            rect, Color.FromArgb(34, 197, 94), Color.FromArgb(22, 163, 74), 45f))
+                        {
+                            canvas.FillEllipse(brush, rect);
+                        }
+                        // Snake eyes
+                        using (SolidBrush eyeBrush = new SolidBrush(Color.White))
+                        {
+                            int eyeSize = Settings.Width / 4;
+                            canvas.FillEllipse(eyeBrush,
+                                Snake[i].X * Settings.Width + Settings.Width / 3,
+                                Snake[i].Y * Settings.Height + Settings.Height / 3,
+                                eyeSize, eyeSize);
+                            canvas.FillEllipse(eyeBrush,
+                                Snake[i].X * Settings.Width + Settings.Width * 2 / 3,
+                                Snake[i].Y * Settings.Height + Settings.Height / 3,
+                                eyeSize, eyeSize);
+                        }
                     }
                     else
                     {
-                        snakeColour = Brushes.Green;
+                        // Body - lighter green with gradient
+                        using (LinearGradientBrush brush = new LinearGradientBrush(
+                            rect, Color.FromArgb(74, 222, 128), Color.FromArgb(134, 239, 172), 45f))
+                        {
+                            canvas.FillEllipse(brush, rect);
+                        }
                     }
 
-                    //Drawing of the Snake and its body
-                    canvas.FillEllipse(snakeColour,
-                                            new Rectangle(
-                                                Snake[i].X * Settings.Width,
-                                                Snake[i].Y * Settings.Height,
-                                                Settings.Width, Settings.Height
-                                                ));
+                    // Add outline
+                    using (Pen pen = new Pen(Color.FromArgb(22, 163, 74), 2))
+                    {
+                        canvas.DrawEllipse(pen, rect);
+                    }
                 }
 
-                //Drawing of the food
-                canvas.FillEllipse(Brushes.Red,
-                                    new Rectangle(
-                                        food.X * Settings.Width,
-                                        food.Y * Settings.Height,
-                                        Settings.Width, Settings.Height
-                                        ));
+                // Drawing of the regular food with green gradient
+                Rectangle foodRect = new Rectangle(
+                    food.X * Settings.Width,
+                    food.Y * Settings.Height,
+                    Settings.Width, Settings.Height);
+
+                using (LinearGradientBrush brush = new LinearGradientBrush(
+                    foodRect, Color.FromArgb(239, 68, 68), Color.FromArgb(220, 38, 38), 45f))
+                {
+                    canvas.FillEllipse(brush, foodRect);
+                }
+                using (Pen pen = new Pen(Color.FromArgb(185, 28, 28), 2))
+                {
+                    canvas.DrawEllipse(pen, foodRect);
+                }
+
+                // Draw bonus food if active
+                if (bonusFoodActive)
+                {
+                    Rectangle bonusRect = new Rectangle(
+                        bonusFood.X * Settings.Width - Settings.Width / 4,
+                        bonusFood.Y * Settings.Height - Settings.Height / 4,
+                        (int)(Settings.Width * 1.5), (int)(Settings.Height * 1.5));
+
+                    using (LinearGradientBrush brush = new LinearGradientBrush(
+                        bonusRect, Color.FromArgb(251, 191, 36), Color.FromArgb(245, 158, 11), 45f))
+                    {
+                        canvas.FillEllipse(brush, bonusRect);
+                    }
+                    using (Pen pen = new Pen(Color.FromArgb(217, 119, 6), 3))
+                    {
+                        canvas.DrawEllipse(pen, bonusRect);
+                    }
+
+                    // Draw sparkle effect
+                    using (Pen sparkle = new Pen(Color.White, 2))
+                    {
+                        int centerX = bonusFood.X * Settings.Width + Settings.Width / 2;
+                        int centerY = bonusFood.Y * Settings.Height + Settings.Height / 2;
+                        canvas.DrawLine(sparkle, centerX - 5, centerY, centerX + 5, centerY);
+                        canvas.DrawLine(sparkle, centerX, centerY - 5, centerX, centerY + 5);
+                    }
+                }
             }
             else
             {
-                //For when the game is over
+                // Better looking game over screen with modern feel
+                string gameOver = "GAME OVER";
+                string score = $"Score: {Settings.Score}";
+                string restart = "Press ENTER to restart";
 
-                string gameOver = "Game Over \n" + "Your Final Score is " + Settings.Score + "\n Press enter to Restart \n";
-                label3.Text = gameOver;
-                label3.Visible = true;
+                Font titleFont = new Font("Segoe UI", 32, FontStyle.Bold);
+                Font scoreFont = new Font("Segoe UI", 24, FontStyle.Regular);
+                Font restartFont = new Font("Segoe UI", 14, FontStyle.Regular);
+
+                SizeF gameOverSize = canvas.MeasureString(gameOver, titleFont);
+                SizeF scoreSize = canvas.MeasureString(score, scoreFont);
+                SizeF restartSize = canvas.MeasureString(restart, restartFont);
+
+                float centerX = pictureBox1.Width / 2;
+                float centerY = pictureBox1.Height / 2;
+
+                using (SolidBrush brush = new SolidBrush(Color.FromArgb(239, 68, 68)))
+                {
+                    canvas.DrawString(gameOver, titleFont, brush,
+                        centerX - gameOverSize.Width / 2, centerY - 80);
+                }
+
+                using (SolidBrush brush = new SolidBrush(Color.White))
+                {
+                    canvas.DrawString(score, scoreFont, brush,
+                        centerX - scoreSize.Width / 2, centerY - 20);
+                    canvas.DrawString(restart, restartFont, brush,
+                        centerX - restartSize.Width / 2, centerY + 40);
+                }
             }
         }
 
@@ -94,21 +188,22 @@ namespace Snake_Game
             Snake.Add(head);
 
             label2.Text = Settings.Score.ToString();
+            bonusFoodActive = false;
+            bonusFoodTimer = 0;
 
             generateFood();
-
         }
 
         private void movePlayer()
         {
-            // Move body segments first (from tail to neck)
+            // Move body segments first
             for (int i = Snake.Count - 1; i > 0; i--)
             {
                 Snake[i].X = Snake[i - 1].X;
                 Snake[i].Y = Snake[i - 1].Y;
             }
 
-            // Then move the head
+            // Move the head
             switch (Settings.Direction)
             {
                 case Directions.Right:
@@ -125,27 +220,15 @@ namespace Snake_Game
                     break;
             }
 
-            // Boundary wrapping - snake comes out the other side
+            // Boundary wrapping
             int maxXpos = pictureBox1.Size.Width / Settings.Width;
             int maxYpos = pictureBox1.Size.Height / Settings.Height;
 
-            if (Snake[0].X < 0)
-            {
-                Snake[0].X = maxXpos - 1;
-            }
-            else if (Snake[0].X >= maxXpos)
-            {
-                Snake[0].X = 0;
-            }
+            if (Snake[0].X < 0) Snake[0].X = maxXpos - 1;
+            else if (Snake[0].X >= maxXpos) Snake[0].X = 0;
 
-            if (Snake[0].Y < 0)
-            {
-                Snake[0].Y = maxYpos - 1;
-            }
-            else if (Snake[0].Y >= maxYpos)
-            {
-                Snake[0].Y = 0;
-            }
+            if (Snake[0].Y < 0) Snake[0].Y = maxYpos - 1;
+            else if (Snake[0].Y >= maxYpos) Snake[0].Y = 0;
 
             // Self-collision detection
             for (int j = 1; j < Snake.Count; j++)
@@ -156,10 +239,30 @@ namespace Snake_Game
                 }
             }
 
-            // Food detection
+            // Regular food detection
             if (Snake[0].X == food.X && Snake[0].Y == food.Y)
             {
                 eat();
+            }
+
+            // Bonus food detection
+            if (bonusFoodActive && Snake[0].X == bonusFood.X && Snake[0].Y == bonusFood.Y)
+            {
+                eatBonusFood();
+            }
+
+            // Bonus food timer management
+            bonusFoodTimer++;
+            if (bonusFoodTimer >= 100 && !bonusFoodActive) // Spawn bonus food every ~5 seconds
+            {
+                generateBonusFood();
+                bonusFoodTimer = 0;
+            }
+
+            if (bonusFoodActive && bonusFoodTimer >= 150) // Bonus food disappears after ~7.5 seconds
+            {
+                bonusFoodActive = false;
+                bonusFoodTimer = 0;
             }
         }
 
@@ -168,8 +271,16 @@ namespace Snake_Game
             int maxXpos = pictureBox1.Size.Width / Settings.Width;
             int maxYpos = pictureBox1.Size.Height / Settings.Height;
 
-            Random rnd = new Random();
             food = new Circle { X = rnd.Next(0, maxXpos), Y = rnd.Next(0, maxYpos) };
+        }
+
+        private void generateBonusFood()
+        {
+            int maxXpos = pictureBox1.Size.Width / Settings.Width;
+            int maxYpos = pictureBox1.Size.Height / Settings.Height;
+
+            bonusFood = new Circle { X = rnd.Next(0, maxXpos), Y = rnd.Next(0, maxYpos) };
+            bonusFoodActive = true;
         }
 
         private void eat()
@@ -186,6 +297,21 @@ namespace Snake_Game
             generateFood();
         }
 
+        private void eatBonusFood()
+        {
+            Circle body = new Circle
+            {
+                X = Snake[Snake.Count - 1].X,
+                Y = Snake[Snake.Count - 1].Y
+            };
+
+            Snake.Add(body);
+            Settings.Score += Settings.Points * 5; // 5x points for bonus food
+            label2.Text = Settings.Score.ToString();
+            bonusFoodActive = false;
+            bonusFoodTimer = 0;
+        }
+
         private void die()
         {
             Settings.GameOver = true;
@@ -193,30 +319,28 @@ namespace Snake_Game
 
         private void updateScreen(object sender, EventArgs e)
         {
-            //Each tick of the timer runs this very function 
-
             if (Settings.GameOver == true)
             {
                 if (Input.KeyPress(Keys.Enter))
                 {
-                    startGame(); //If the game is over and enter is pressed then start the game
+                    startGame();
                 }
             }
-            else //If the game is not over then the following must be done
+            else
             {
-                if(Input.KeyPress(Keys.Right) && Settings.Direction != Directions.Left)
+                if (Input.KeyPress(Keys.Right) && Settings.Direction != Directions.Left)
                 {
                     Settings.Direction = Directions.Right;
                 }
-                else if(Input.KeyPress(Keys.Left) && Settings.Direction != Directions.Right)
+                else if (Input.KeyPress(Keys.Left) && Settings.Direction != Directions.Right)
                 {
                     Settings.Direction = Directions.Left;
                 }
-                else if(Input.KeyPress(Keys.Up) && Settings.Direction != Directions.Down)
+                else if (Input.KeyPress(Keys.Up) && Settings.Direction != Directions.Down)
                 {
                     Settings.Direction = Directions.Up;
                 }
-                else if(Input.KeyPress(Keys.Down) && Settings.Direction != Directions.Up)
+                else if (Input.KeyPress(Keys.Down) && Settings.Direction != Directions.Up)
                 {
                     Settings.Direction = Directions.Down;
                 }
